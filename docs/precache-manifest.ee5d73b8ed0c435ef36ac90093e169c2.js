fb94efd0d68743ef2a8f243bd0817e84239748fe
OSantiagoPerezE <ospereze98@gmail.com>
2021-11-12 10:24:14 -0500

    Docs

---
+++ docs/precache-manifest.ee5d73b8ed0c435ef36ac90093e169c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86b7f94b5d2e7d3903696aee137fb7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43a9ee9d1e65bba4cb0b",
+    "url": "/static/js/2.2ece8b1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2ece8b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc1916241202b532ebd",
+    "url": "/static/js/main.ef3026b3.chunk.js"
+  },
+  {
+    "revision": "ede06adfc8d41a12f0e7",
+    "url": "/static/js/runtime-main.c607270d.js"
+  }
+]);
