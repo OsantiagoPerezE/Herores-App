44118d2aa626ae47bd4e0f905c8135712c811b13
OSantiagoPerezE <ospereze98@gmail.com>
2021-11-13 20:48:35 -0500

    Docs agregado

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import { AuthContext } from '../auth/AuthContext';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
+	const { user } = useContext(AuthContext);
+
 	return (
 		<Router>
 			<div>
 				<Switch>
-					<Route exact path='/login' component={LoginScreen} />
+					<PublicRoute exact path='/login' component={LoginScreen} isAuthenticated={user.logged} />
 
-					<Route path='/' component={DashboardRoutes} />
+					<PrivateRoute path='/' component={DashboardRoutes} isAuthenticated={user.logged} />
 				</Switch>
 			</div>
 		</Router>
