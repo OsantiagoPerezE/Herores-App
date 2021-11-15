f66d0e79db4079b83e0f557c13e9c8b33ef1ef77
OSantiagoPerezE <ospereze98@gmail.com>
2021-11-15 09:25:37 -0500

    test agregado

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Redirect, Route } from 'react-router-dom';
+
+import { Route, Redirect } from 'react-router-dom';
 
 export const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => {
+	localStorage.setItem('lastPath', rest.location.pathname);
+
 	return (
 		<Route
 			{...rest}
@@ -11,7 +14,7 @@ export const PrivateRoute = ({ isAuthenticated, component: Component, ...rest })
 	);
 };
 
-PrivateRoute.prototype = {
+PrivateRoute.propTypes = {
 	isAuthenticated: PropTypes.bool.isRequired,
 	component: PropTypes.func.isRequired,
 };
